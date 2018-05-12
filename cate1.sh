672393bd2dce51888f1920c9f606a88517beeef2
lbj <2105931798@qq.com>
2018-05-12 22:02:31 +0800

    shell脚本实现按条件批量更新

--- cate1.sh
+++ cate1.sh
@@ -1,2 +1,19 @@
-length=cat cate1.json | jq '.|length' 
-echo $length
+a=`cat cate2.json | jq '.|length'`
+echo ${a}
+for((i=0;i<${a};i++))
+do
+tmp=`cat cate2.json | jq ".[${i}].parentid"`
+echo ${tmp}
+if((${tmp}==0))
+then
+continue
+else
+id=`cat cate2.json | jq ".[${i}].id"`
+oid=`cat cate2.json | jq ".[${i}].objectId"`
+pObjectId=`cat cate2.json | jq ".[${id}].objectId"`
+echo "id:${id}"
+echo "oid:${oid}"
+echo "pid:${pObjectId}"
+sh curl -X PUT \-H "X-Parse-Application-Id: bec" \-H "Content-Type: application/json" \ -d "{"pointerCategory": {"__type": "Pointer","className": "Category","objectId": ${pObjectId}}}" \ http://localhost:1337/parse/classes/Category/"${id}"
+fi
+done
