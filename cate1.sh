27a337e830f5e9d4fd0b0d6677f173c8dc1bdacf
lbj <2105931798@qq.com>
2018-05-17 12:24:44 +0800

    category服务器parse脚本，以及分类数据脚本升级

--- cate1.sh
+++ cate1.sh
@@ -9,11 +9,32 @@ then
 continue
 else
 id=`cat cate2.json | jq ".[${i}].id"`
-oid=`cat cate2.json | jq ".[${i}].objectId"`
-pObjectId=`cat cate2.json | jq ".[${id}].objectId"`
+oid=`cat cate2.json | jq ".[${i}]".objectId`
+ojid=`cat cate2.json | jq ".[${i}]".objectId | tr -d '"'`
+echo ${ojid}
+pObjectId=`cat cate2.json | jq ".[${tmp}].objectId"`
+for((k=0;k<${a};k++))
+do 
+kid=`cat cate2.json | jq ".[${k}].id"`
+if((${kid}==${tmp}))
+then
+parentOid=`cat cate2.json | jq ".[${k}].objectId"`
+else
+continue
+fi
+done
+echo ${parentOid}
 echo "id:${id}"
 echo "oid:${oid}"
 echo "pid:${pObjectId}"
-sh curl -X PUT \-H "X-Parse-Application-Id: bec" \-H "Content-Type: application/json" \ -d "{"pointerCategory": {"__type": "Pointer","className": "Category","objectId": ${pObjectId}}}" \ http://localhost:1337/parse/classes/Category/"${id}"
+curl -X PUT \
+  -H "X-Parse-Application-Id: bec" \
+  -H "Content-Type: application/json" \
+  -d '{"pointerCategory": {
+				"__type": "Pointer",
+				"className": "Category",
+				"objectId": '"${parentOid}"'
+			}}' \
+http://localhost:1337/parse/classes/Category/"${ojid}"
 fi
 done
